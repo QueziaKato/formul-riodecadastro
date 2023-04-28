9890bf1b1e8e2c4e250c4152b4b2c9f6199c76d9
Quezia <quezia.kato@alunoescolassociais.org.br>
2023-04-28 10:36:02 -0300

    Finalizado

--- cadastro.js
+++ cadastro.js
@@ -3,33 +3,24 @@
 //Código de validação de formulário de Cadastro
 
 function verificar() {
-   let nome = document.getElementById("Nome").value;
-   let number = document.getElementById("Data de Nascimento").value;
-   let cpf = document.getElementById("CPF").value;
-   let email = document.getElementById("Email").value;
-   let telefone = document.getElementById("Telefone").value;
-   let cep = document.getElementById("CEP").value
-   let endereço = document.getElementById("Endereço").value
-   let número = document.getElementById("Número").value;
-   let complemento = document.getElementById("Complemento").value;
-   let bairro = document.getElementById("Bairro").value;
-   let estado = document.getElementById("Estado").value;
-    let ponto= document.getElementById ("Ponto de referência").value;
-   let senha = document.getElementById("Senha").value;
-   let confirmação = document.getElementById("Confirmação de Senha").value;
-    
-if (!email || !senha || !nome || !dtnasc ||!Complemento) {
-    alert("Campos de preenchimento obrigatório. Favor preencher");
-    
-    } else {
-    alert("Campos preenchidos com sucesso!");
-    
-}
-    
-    
-    
-    
-    
-    
-    
-    }
+     let Nome = document.getElementById('nome').value;
+     let DatadeNascimento = document.getElementById('dtnasc').value;
+     let CPF = document.getElementById('CPF').value;
+     let Email = document.getElementById('email').value;
+     let telefone = document.getElementById('telefone').value;
+     let CEP = document.getElementById('CEP').value;
+     let Endereço = document.getElementById('Endereco').value;
+     let Numero = document.getElementById('Numero').value;
+     let Complemento = document.getElementById('Complemento').value;
+     let Bairro = document.getElementById('Bairro').value;
+     let Estado = document.getElementById('Estado').value;
+     let Ptr = document.getElementById('Ptr').value;
+     let confirmSenha = document.getElementById('confirmsenha').value;
+     let senha = document.getElementById('Senha').value; 
+     {
+     if (!Nome || !DatadeNascimento || !CPF || !Email || !telefone || !CEP || !Endereço || !Numero || !Complemento || !Bairro || !Estado || !Ptr || !confirmSenha || !senha) {
+    alert("Campo de preenchimento obrigatório");
+     }else
+     alert("Campos preenchidos com sucesso!");
+     }
+ }
